7f7eaaf09aef39b8ae15500fbe8af2a46cbc5e47
cr-mao <crmao@qq.com>
2023-01-04 17:55:31 +0800

    爬虫加上代理的翅膀

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/cr-mao/crawler
+
+go 1.18
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	golang.org/x/net v0.4.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/cr-mao/crawler/collect"
+	"time"
+)
+
+func main() {
+	//proxyURLs := []string{"http://127.0.0.1:8888", "http://127.0.0.1:8888"}
+	//p, err := proxy.RoundRobinProxySwitcher(proxyURLs...)
+	//if err != nil {
+	//	fmt.Println("RoundRobinProxySwitcher failed")
+	//}
+	url := "https://google.com"
+	var f collect.Fetcher = collect.BrowserFetch{
+		Timeout: 3000 * time.Millisecond,
+		//Proxy:   p,
+	}
+	body, err := f.Get(url)
+	if err != nil {
+		fmt.Printf("read content failed:%v\n", err)
+		return
+	}
+	fmt.Println(string(body))
+	// 加载HTML文档
+	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(body))
+	if err != nil {
+		fmt.Println("read content failed:%v", err)
+	}
+	doc.Find("div.news_li h2 a[target=_blank]").Each(func(i int, s *goquery.Selection) {
+		// 获取匹配元素的文本
+		title := s.Text()
+		fmt.Printf("Review %d: %s\n", i, title)
+	})
+}
